5ff0b6cadf3b079b852d5eac7687a3d96bf1ef89
RABBANI-N <nawras.rabbani@mail.mcgill.ca>
2016-02-25 17:11:42 -0500

    modified main

--- src/finalProject/main.java
+++ src/finalProject/main.java
@@ -1,5 +1,25 @@
 package finalProject;
 
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.Port;
+
 public class main {
+
+	// Robot Parameters
+	private static double WHEEL_RADIUS = 2.1;
+	private static double WHEEL_WIDTH = 13.5;
+
+	// Instatiate Motors
+	private static final EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
+	private static final EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("C"));
+
+	// Instantiate Ports
+	private static final Port usPort = LocalEV3.get().getPort("S1");
+	private static final Port colorPort = LocalEV3.get().getPort("S2");
+
+	public static void main(String[] args) {
+
+	}
 
 }
