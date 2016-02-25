71659bd354ddfe6af320d43a3ff5b3afb535ec62
Tiffany Wang <TiffanyWang@wpa146063.wireless.mcgill.ca>
2016-02-25 17:18:22 -0500

    add comment

--- src/finalProject/main.java
+++ src/finalProject/main.java
@@ -1,7 +1,7 @@
 package finalProject;
 
 import lejos.hardware.ev3.LocalEV3;
-import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3LargeRegulatedMotor; // heiii
 import lejos.hardware.port.Port;
 
 public class main {
